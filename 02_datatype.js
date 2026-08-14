5dd79211c99e71119615e04d6a1414639a32c5b8
Awais <awaismalik781199@gmail.com>
2026-08-14 10:39:06 +0500

    datatype

---
+++ 02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // Treat all the code as newer version
+
+// alert("thats a alert") we are using node js not the browser
+
+let name = "Awais";
+let age = 18;
+let isloggedin = false;
+// Number 2 to power of 53
+
+//biginit
+
+//string use in single and double quotes
+
+// boolean => true or false 
+
+//null => Acutally its a stand alone value and also a data type in javasript.Its a reprisentation of empty value.Its a special data type
+
+// Undifined => when the value is not defined example: variable is defined but  does not assign any value to it
+
+//symbol => use to define the uniqueness of elements 
+
+//Object
+
+// If we want to find the data type of any thing;
+console.log(typeof "");
+
+// Its very important ;
+console.log(typeof null); // object
